2cc136944821486bb5da25af4a4d82543e829fcd
zapaz.eth <alain@kredeum.com>
2022-07-25 18:37:23 +0200

    first commit from Kredeum/kredeum

---
+++ src/interfaces/IOpenBound.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IOpenBound {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        address owner,
+        uint256 maxSupply
+    ) external;
+
+    function mint(uint256 tokenID) external returns (uint256);
+
+    function claim(uint256 tokenID, uint256 cid) external;
+
+    function burn(uint256 tokenID) external;
+
+    function getMyTokenID(uint256 cid) external view returns (uint256);
+
+    function getCID(uint256 tokenID) external view returns (uint256);
+}
